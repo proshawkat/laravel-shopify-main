2da179f3358613e2adfb004c21586cf092db35ca
shawkat <shawkat.ali@netmediamobile.com>
2022-07-20 21:14:25 +0600

    shopify authentication

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware(['verify.shopify'])->name('home');
+
+//This will redirect user to login page.
+Route::get('/login', function () {
+    if (Auth::user()) {
+        return redirect()->route('home');
+    }
+    return view('login');
+})->name('login');
